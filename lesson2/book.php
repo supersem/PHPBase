f0a75718b6d8804ce43544d4119551175b6d0f19
supersem <xsupersemx@gmail.com>
2022-04-10 22:00:30 +0300

    add constructor

--- lesson2/book.php
+++ lesson2/book.php
@@ -1,22 +1,19 @@
 <?php
-class Book {
- 
-  public $title = "Title";
-  public $author = "Author";
-  public $price = 0;
-
-  function getBook () {
-    echo "Title:".$this->title." Author:".$this->author." Price:".$this->price;
-  }  
+class Book { 
+  function __construct($title = "", $author = "", $price = 0) {
+    $this->title = $title;
+    $this->author = $author;
+    $this->price = $price;
+  }
+  
+  function getBook() {
+   echo "Title:".$this->title." Author:".$this->author." Price:".$this->price;
+  }
+  
 }
 
 $book1 = new Book();
-$book2 = new Book();
+$book2 = new Book('PHP objects', 'Zandstra', 100);
 
 $book1->getBook();
-
-$book2->title = 'PHP objects';
-$book2->author = 'Zandstra';
-$book2->price = 100;
-
 $book2->getBook();
