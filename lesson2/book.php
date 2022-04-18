c67374bdde263f0ff6d9cea6ae4ceddb49b2546e
supersem <xsupersemx@gmail.com>
2022-04-18 22:11:12 +0300

    add third party package using composer

--- lesson2/book.php
+++ lesson2/book.php
@@ -1,4 +1,9 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
 class Book { 
   function __construct($title = "", $author = "", $price = 0) {
     $this->title = $title;
@@ -17,3 +22,26 @@ $book2 = new Book('PHP objects', 'Zandstra', 100);
 
 $book1->getBook();
 $book2->getBook();
+
+
+
+$slugify = new Slugify();
+
+$data = $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
+echo $data;
+
+function my_error_handler($str) {
+  if (!strpos($str, "-")) {  
+    throw new Exception("Data should includes '-'");  
+  }  
+  return true;  
+}  
+
+try {  
+  my_error_handler($data);  
+  echo "If you see this text, the passed data includes '-'";  
+}  
+ 
+catch (Exception $e) {  
+  echo 'Exception Message: ' .$e->getMessage();  
+}  
